12d23f35145f8ff1d3bd160117fb52ab7bcc7d0a
Jorge Rivera Mancilla <jiriveramancilla@gmail.com>
2025-09-10 01:34:00 -0300

    chore(project): add prettier configuration, husky hooks, and new components

---
+++ src/components/shadcn/dialog.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+import {
+  Close,
+  Content,
+  Description,
+  Overlay,
+  Portal,
+  Root,
+  Title,
+  Trigger,
+} from '@radix-ui/react-dialog';
+import { XIcon } from 'lucide-react';
+
+import type { ComponentProps } from 'react';
+
+import { cn } from '@/libs';
+
+export const Dialog = ({ ...props }: ComponentProps<typeof Root>) => {
+  return <Root data-slot="dialog" {...props} />;
+};
+
+export const DialogTrigger = ({ ...props }: ComponentProps<typeof Trigger>) => {
+  return <Trigger data-slot="dialog-trigger" {...props} />;
+};
+
+export const DialogPortal = ({ ...props }: ComponentProps<typeof Portal>) => {
+  return <Portal data-slot="dialog-portal" {...props} />;
+};
+
+export const DialogClose = ({ ...props }: ComponentProps<typeof Close>) => {
+  return <Close data-slot="dialog-close" {...props} />;
+};
+
+export const DialogOverlay = ({
+  className,
+  ...props
+}: ComponentProps<typeof Overlay>) => {
+  return (
+    <Overlay
+      data-slot="dialog-overlay"
+      className={cn(
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50',
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const DialogContent = ({
+  className,
+  children,
+  showCloseButton = true,
+  ...props
+}: ComponentProps<typeof Content> & {
+  showCloseButton?: boolean;
+}) => {
+  return (
+    <DialogPortal data-slot="dialog-portal">
+      <DialogOverlay />
+      <Content
+        data-slot="dialog-content"
+        className={cn(
+          'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        {showCloseButton && (
+          <Close
+            data-slot="dialog-close"
+            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+          >
+            <XIcon />
+            <span className="sr-only">Close</span>
+          </Close>
+        )}
+      </Content>
+    </DialogPortal>
+  );
+};
+
+export const DialogHeader = ({
+  className,
+  ...props
+}: ComponentProps<'div'>) => {
+  return (
+    <div
+      data-slot="dialog-header"
+      className={cn('flex flex-col gap-2 text-center sm:text-left', className)}
+      {...props}
+    />
+  );
+};
+
+export const DialogFooter = ({
+  className,
+  ...props
+}: ComponentProps<'div'>) => {
+  return (
+    <div
+      data-slot="dialog-footer"
+      className={cn(
+        'flex flex-col-reverse gap-2 sm:flex-row sm:justify-end',
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const DialogTitle = ({
+  className,
+  ...props
+}: ComponentProps<typeof Title>) => {
+  return (
+    <Title
+      data-slot="dialog-title"
+      className={cn('text-lg leading-none font-semibold', className)}
+      {...props}
+    />
+  );
+};
+
+export const DialogDescription = ({
+  className,
+  ...props
+}: ComponentProps<typeof Description>) => {
+  return (
+    <Description
+      data-slot="dialog-description"
+      className={cn('text-muted-foreground text-sm', className)}
+      {...props}
+    />
+  );
+};
---
+++ src/components/shadcn/tooltip.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import {
+  Arrow,
+  Content,
+  Portal,
+  Provider,
+  Root,
+  Trigger,
+} from '@radix-ui/react-tooltip';
+
+import type { ComponentProps } from 'react';
+
+import { cn } from '@/libs';
+
+export const TooltipProvider = ({
+  delayDuration = 0,
+  ...props
+}: ComponentProps<typeof Provider>) => {
+  return (
+    <Provider
+      data-slot="tooltip-provider"
+      delayDuration={delayDuration}
+      {...props}
+    />
+  );
+};
+
+export const Tooltip = ({ ...props }: ComponentProps<typeof Root>) => {
+  return (
+    <TooltipProvider>
+      <Root data-slot="tooltip" {...props} />
+    </TooltipProvider>
+  );
+};
+
+export const TooltipTrigger = ({
+  ...props
+}: ComponentProps<typeof Trigger>) => {
+  return <Trigger data-slot="tooltip-trigger" {...props} />;
+};
+
+export const TooltipContent = ({
+  className,
+  sideOffset = 0,
+  children,
+  ...props
+}: ComponentProps<typeof Content>) => {
+  return (
+    <Portal>
+      <Content
+        data-slot="tooltip-content"
+        sideOffset={sideOffset}
+        className={cn(
+          'bg-primary text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) rounded-md px-3 py-1.5 text-xs text-balance',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        <Arrow className="bg-primary fill-primary z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px]" />
+      </Content>
+    </Portal>
+  );
+};
