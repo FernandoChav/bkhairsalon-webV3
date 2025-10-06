ddd9a437da89e7b1a8a196c970c2ebb2640e2552
Jorge Rivera Mancilla <jiriveramancilla@gmail.com>
2025-10-06 10:40:37 -0300

    refactor(protected-route): extract session management and authentication error handling into a custom hook for improved clarity and reusability

--- src/hooks/common/index.ts
+++ src/hooks/common/index.ts
@@ -1,4 +1,5 @@
 export { useFileUpload } from './use-file-upload';
 export { useIsMobile } from './use-mobile';
 export { useNavigationBar } from './use-navigation-bar';
+export { useProtectedRouteWrapper } from './use-protected-route-wrapper';
 export { useProgress } from './use-progress';
