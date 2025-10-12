70ad74f219bcc403eaf189ae839d04420c8439e5
Jorge Rivera Mancilla <jiriveramancilla@gmail.com>
2025-10-11 23:43:08 -0300

    chore(dependencies): update package dependencies and dialog component configuration

--- src/components/shadcn/dialog.tsx
+++ src/components/shadcn/dialog.tsx
@@ -52,9 +52,11 @@ export const DialogContent = ({
   className,
   children,
   showCloseButton = true,
+  closeButtonClassName,
   ...props
 }: ComponentProps<typeof Content> & {
   showCloseButton?: boolean;
+  closeButtonClassName?: string;
 }) => {
   return (
     <DialogPortal data-slot="dialog-portal">
@@ -71,7 +73,10 @@ export const DialogContent = ({
         {showCloseButton && (
           <Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className={cn(
+              "ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer",
+              closeButtonClassName
+            )}
           >
             <HiX />
             <span className="sr-only">Close</span>
