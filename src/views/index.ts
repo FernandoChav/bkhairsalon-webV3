eb2327376f518b2ab11eaecb90b1e3738fcf7d54
godchisa254 <Matias.254@outlook.com>
2025-10-24 05:02:58 -0300

    refactor(routing): no more userspace/service, only /service from now on

--- src/views/index.ts
+++ src/views/index.ts
@@ -5,4 +5,4 @@ export { WorkspaceView } from './app/workspace';
 export { ServiceView } from './app/workspace/service';
 export { AccountView } from './app/account';
 export { EditUserView } from './app/account/edit';
-export { CustomerServiceView } from './app/userspace/service';
+export { CustomerServiceView } from './app/service';
