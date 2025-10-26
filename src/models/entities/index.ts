6743ce5eaebebcd4df1b73d9c595ab53ed9e1bc6
godchisa254 <Matias.254@outlook.com>
2025-10-26 02:07:57 -0300

    feat(interface): adds interface for animated card component

--- src/models/entities/index.ts
+++ src/models/entities/index.ts
@@ -3,3 +3,4 @@ export { UserRole } from './role';
 export type { Category } from './category';
 export type { Service } from './service';
 export type { CartItem } from './shopping-cart-item';
+export type { FlyingCard } from './flying-card';
